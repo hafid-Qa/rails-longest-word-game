b20c81f1d5d9fc86aedc70e098b4bc2b22f68a09
Hafid Qarchi <haqarchi@gmail.com>
2022-11-07 12:52:31 +0900

    run game logic

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,4 +10,17 @@ class GamesController < ApplicationController
   def generate_grid
     ('a'..'z').to_a.sample(9)
   end
+
+  def run_game(attempt, grid)
+    url = "https://wagon-dictionary.herokuapp.com/#{attempt}"
+    attempt_serialized = URI.open(url).read
+    attempt_json = JSON.parse(attempt_serialized)['found']
+    final_result = {
+      attempt:,
+      # time: end_time - start_time,
+      score: 0,
+      message: "<strong>Congratulatons</strong> #{@attempt.upcase} is a valid English word!"
+    }
+    display_result(final_result, attempt_json, grid, attempt)
+  end
 end
